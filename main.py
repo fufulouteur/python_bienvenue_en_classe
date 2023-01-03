e279d18bceabd27fceb66eca1435918372cbdef3
fufulouteur <manzodqb@icloud.com>
2023-01-03 18:29:20 -0500

    pojet fini bienvenue en classe

---
+++ main.py
@@ -0,0 +1,22 @@
+print('Bienvenue! reponds a ces question pour te presenter.')
+
+print('quel est ton prenom ?')
+prenom = input()
+print(prenom)
+
+print('ta couleur favorite ?')
+couleur = input()
+print(couleur)
+
+print('ce que tu preferes manger ?')
+plat = input()
+print(plat)
+
+print('le film favori ?')
+film = input()
+print(film)
+
+print(F"je vous presente {prenom}! " +
+      F" {prenom} adore la couleur {couleur}. " +
+      F"son plat prefere est {plat}. " +
+      F"son film favori est {film}. ")
