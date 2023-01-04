0b362a2a883f090640df63f6f87ee956602f3047
fufulouteur <manzodqb@icloud.com>
2023-01-03 19:34:06 -0500

    pojet fini bienvenue en classe

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ print('quel est ton prenom ?')
 prenom = input()
 print(prenom)
 
+
 print('ta couleur favorite ?')
 couleur = input()
 print(couleur)
@@ -15,8 +16,21 @@ print(plat)
 print('le film favori ?')
 film = input()
 print(film)
+if film == ('pokemon'):
+    print('question bonus')
+    print('jeux video prefere')
+    jeux_video = input()
+    print(jeux_video)
 
 print(F"je vous presente {prenom}! " +
       F" {prenom} adore la couleur {couleur}. " +
       F"son plat prefere est {plat}. " +
       F"son film favori est {film}. ")
+if prenom == ('fufulouteur') + couleur == ('red') + plat == ('sushi') + film == ('pokemon'):
+    print(F" je vous presente le grand, le beau {prenom}! " +
+          F" {prenom} adore la couleur {couleur}car c'est une couleur vive. " +
+          F" son plat prefere est les {plat} par ce que il aime le poison . " +
+          F" il adore {film}, ")
+
+
+
